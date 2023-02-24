8b91317a44956880b1cc041016490f99c5e166d6
Mohammed Moahmmed Awwal <abbaroze@gmail.com>
2023-02-24 19:56:58 +0100

    Server creation

---
+++ server/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
+import morgan from "morgan";
+import helmet from "helmet";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// CONFIGURATIONS
