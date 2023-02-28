7dcfc26e531195cfdf052acaf17ca09beb297dab
Mohammed Moahmmed Awwal <abbaroze@gmail.com>
2023-02-28 21:40:59 +0100

    added the finishing touches to server(routes,etc)

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,20 +1,21 @@
-import { Jwt } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 
 export const verifyToken = async (req, res, next) => {
- try {
-  let token = req.header("Authorization");
+  try {
+    let token = req.header("Authorization");
 
-  if (!token) {
-   return res.status(403).send("Access Denied");
-  }
-  if (token.startsWIth("Bearer ")){
-   token = token.slice(7, token.length).trimLeft();
-  }
+    if (!token) {
+      return res.status(403).send("Access Denied");
+    }
 
-  const verified = jwt.verify(token, process.env.JWT_SECRET);
-  req.user = verified;
-  next();
- } catch (err) {
-  res.status(500).json({ error: err.message });
- }
-}
+    if (token.startsWith("Bearer ")) {
+      token = token.slice(7, token.length).trimLeft();
+    }
+
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = verified;
+    next();
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
