09b9d895d8c015a721633acf8489a498763eeeec
Mohammed Moahmmed Awwal <abbaroze@gmail.com>
2023-02-28 10:42:04 +0100

    edit

--- server/index.js
+++ server/index.js
@@ -11,6 +11,9 @@ import { fileURLToPath } from "url";
 import {register} from "./controllers/auth.js";
 import userRoutes from "./routes/users.js"
 import authRoutes from "./routes/auth.js"
+import postRoutes from "./routes/posts.js"
+import { createPost} from "./controllers/posts.js"
+import { verifyToken } from "./middleware/auth.js";
 
 // CONFIGURATIONS
 const __filename = fileURLToPath(import.meta.url);
@@ -38,11 +41,13 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 // ROUTES WITH FILS
-app.post("auth/register", upload.single("picture"), register)
+app.post("auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost)
 
 // ROUTES
 app.use("/auth", authRoutes)
 app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 // MONGOOSE SETUP
 const PORT  = process.env.PORT || 6001;
